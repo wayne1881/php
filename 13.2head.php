9b346b143fa9fb5e7b8c6cc85783167774760f57
shhuangmust <63779526+shhuangmust@users.noreply.github.com>
2020-12-25 13:10:54 +0800

    Add files via upload

---
+++ 13.2head.php
@@ -0,0 +1,6 @@
+<html>
+    <head>
+        <title>佈告管理介面</title>
+    </head>
+    <body>
+    <div style="height: 70px; text-align:center; background-color: gray; line-height:70px"><h1>佈告管理介面</h1></div>
---
+++ 13.3footer.php
@@ -0,0 +1,3 @@
+<div style="height:70px; text-align:center; background-color:lightgray; line-height:70px">版權沒有，歡迎拷貝</div>
+</body>
+</html>
