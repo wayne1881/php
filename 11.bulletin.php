0ae3ff9c44c96c196d452c1235e270eb4be7ef20
shhuangmust <63779526+shhuangmust@users.noreply.github.com>
2020-12-25 13:47:30 +0800

    Update 11.bulletin.php

--- 11.bulletin.php
+++ 11.bulletin.php
@@ -3,7 +3,7 @@
     session_start();
     if (!isset($_SESSION["id"])){
         echo "請登入系統";
-        echo "<meta http-equiv='refresh' content='3; url='login.html'>";
+        echo "<meta http-equiv='refresh' content='3; url=login.html'>";
     }else{
         echo "歡迎 {$_SESSION['id']} 登入 [<a href=logout.php>登出</a>]<p>[<a href=bulletin_add_form.php>新增佈告</a>]<p>";
         $conn=mysqli_connect("localhost","root","", "mydb");
