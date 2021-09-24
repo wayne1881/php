f7c3a8b313ca92c0e6258a7550ad4509f1d85e1e
shhuangmust <63779526+shhuangmust@users.noreply.github.com>
2021-09-24 12:44:01 +0800

    Delete 13.2head.php

--- 13.2head.php
+++
@@ -1,6 +0,0 @@
-<html>
-    <head>
-        <title>佈告管理介面</title>
-    </head>
-    <body>
-    <div style="height: 70px; text-align:center; background-color: gray; line-height:70px"><h1>佈告管理介面</h1></div>
