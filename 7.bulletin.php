a40045256eafbc384368ee1b0764486b652e4829
shhuangmust <63779526+shhuangmust@users.noreply.github.com>
2021-10-15 16:12:28 +0800

    Update 7.bulletin.php

--- 7.bulletin.php
+++ 7.bulletin.php
@@ -5,15 +5,15 @@
     echo "<table border=2><tr><td>佈告編號</td><td>佈告類別</td><td>標題</td><td>佈告內容</td><td>發佈時間</td></tr>";
     while ($row=mysqli_fetch_array($result)){
         echo "<tr><td>";
-        echo $row[bid];
+        echo $row["bid"];
         echo "</td><td>";
-        echo $row[type];
+        echo $row["type"];
         echo "</td><td>"; 
-        echo $row[title];
+        echo $row["title"];
         echo "</td><td>";
-        echo $row[content]; 
+        echo $row["content"]; 
         echo "</td><td>";
-        echo $row[time];
+        echo $row["time"];
         echo "</td></tr>";
     }
     echo "</table>"
