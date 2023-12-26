53042fae1bca25aa9cc2c8058c9660835fc0f42f
shhuangmust <63779526+shhuangmust@users.noreply.github.com>
2023-12-26 12:01:39 +0800

    Update 15.user_add.php

--- 15.user_add.php
+++ 15.user_add.php
@@ -4,12 +4,12 @@ error_reporting(0);
 session_start();
 if (!$_SESSION["id"]) {
     echo "請登入帳號";
-    echo "<meta http-equiv=REFRESH content='3, url=login.html'>";
+    echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
 }
 else{    
 
    #mysqli_connect() 建立資料庫連結
-   $conn=mysqli_connect("localhost","root","","mydb");
+   $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
    #mysqli_query() 從資料庫查詢資料
    #新增資料SQL命令：insert into 表格名稱(欄位1,欄位2) values(欄位1的值,欄位2的值)
    $sql="insert into user(id,pwd) values('{$_POST['id']}', '{$_POST['pwd']}')";
@@ -19,7 +19,7 @@ else{
    }
    else{
      echo "新增使用者成功，三秒鐘後回到網頁";
-     echo "<meta http-equiv=REFRESH content='3, url=bulletin.php'>";
+     echo "<meta http-equiv=REFRESH content='3, url=11.bulletin.php'>";
    }
 }
 ?>
