b409cc61f407bbfdbede42d50e6ed0b9a83d8a91
shhuangmust <63779526+shhuangmust@users.noreply.github.com>
2023-12-26 12:09:03 +0800

    Update 18.user.php

--- 18.user.php
+++ 18.user.php
@@ -6,7 +6,7 @@
         session_start();
         if (!$_SESSION["id"]) {
             echo "請登入帳號";
-            echo "<meta http-equiv=REFRESH content='3, url=login.html'>";
+            echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
         }
         else{   
             echo "<h1>使用者管理</h1>
@@ -14,10 +14,10 @@
                 <table border=1>
                     <tr><td></td><td>帳號</td><td>密碼</td></tr>";
             
-            $conn=mysqli_connect("localhost","root","","mydb");
+            $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
             $result=mysqli_query($conn, "select * from user");
             while ($row=mysqli_fetch_array($result)){
-                echo "<tr><td><a href=user_edit_form.php?id={$row['id']}>修改</a>||<a href=user_delete.php?id={$row['id']}>刪除</a></td><td>{$row['id']}</td><td>{$row['pwd']}</td></tr>";
+                echo "<tr><td><a href=19.user_edit_form.php?id={$row['id']}>修改</a>||<a href=17.user_delete.php?id={$row['id']}>刪除</a></td><td>{$row['id']}</td><td>{$row['pwd']}</td></tr>";
             }
             echo "</table>";
         }
