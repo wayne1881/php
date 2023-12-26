4c73b2da63c4cedcf35c0ed487d655687ff62511
shhuangmust <63779526+shhuangmust@users.noreply.github.com>
2023-12-26 11:39:06 +0800

    Update 7.bulletin.php

--- 7.bulletin.php
+++ 7.bulletin.php
@@ -1,6 +1,6 @@
 <?php
     error_reporting(0);
-    $conn=mysqli_connect("localhost","root","", "mydb");
+    $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
     $result=mysqli_query($conn, "select * from bulletin");
     echo "<table border=2><tr><td>佈告編號</td><td>佈告類別</td><td>標題</td><td>佈告內容</td><td>發佈時間</td></tr>";
     while ($row=mysqli_fetch_array($result)){
