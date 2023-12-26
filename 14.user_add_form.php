5e62f503aa49249ac359fae0e79dff6a155bc6e0
shhuangmust <63779526+shhuangmust@users.noreply.github.com>
2023-12-26 11:59:49 +0800

    Update 14.user_add_form.php

--- 14.user_add_form.php
+++ 14.user_add_form.php
@@ -6,11 +6,11 @@
     session_start();
     if (!$_SESSION["id"]) {
         echo "請登入帳號";
-        echo "<meta http-equiv=REFRESH content='3, url=login.html'>";
+        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
     }
     else{    
         echo "
-            <form action=user_add.php method=post>
+            <form action=15.user_add.php method=post>
                 帳號：<input type=text name=id><br>
                 密碼：<input type=text name=pwd><p></p>
                 <input type=submit value=新增> <input type=reset value=清除>
