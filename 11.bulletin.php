ed42815de1d0bf791aea41aa984709f6c280995b
shhuangmust <63779526+shhuangmust@users.noreply.github.com>
2023-12-26 12:19:49 +0800

    Update 11.bulletin.php

--- 11.bulletin.php
+++ 11.bulletin.php
@@ -9,9 +9,10 @@
         echo "歡迎, ".$_SESSION["id"]."[<a href=12.logout.php>登出</a>][<a href=14.user_add_form.php>新增使用者</a>][<a href=18.user.php>管理使用者</a>] [<a href=22.bulletin_add_form.php>新增佈告</a>]<br>";
         $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
         $result=mysqli_query($conn, "select * from bulletin");
-        echo "<table border=2><tr><td>佈告編號</td><td>佈告類別</td><td>標題</td><td>佈告內容</td><td>發佈時間</td></tr>";
+        echo "<table border=2><tr><td></td><td>佈告編號</td><td>佈告類別</td><td>標題</td><td>佈告內容</td><td>發佈時間</td></tr>";
         while ($row=mysqli_fetch_array($result)){
-            echo "<tr><td>";
+            echo "<tr><td><a href=26.bulletin_edit_form.php?bid={$row["bid"]}>修改</a> 
+            <a href=28.bulletin_delete.php?bid={$row["bid"]}>刪除</a></td><td>";
             echo $row["bid"];
             echo "</td><td>";
             echo $row["type"];
