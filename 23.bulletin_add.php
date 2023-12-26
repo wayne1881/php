eef225984db7203522916f6989e80c1907c9e384
shhuangmust <63779526+shhuangmust@users.noreply.github.com>
2023-12-26 12:43:53 +0800

    Update 23.bulletin_add.php

--- 23.bulletin_add.php
+++ 23.bulletin_add.php
@@ -3,10 +3,10 @@
     session_start();
     if (!$_SESSION["id"]) {
         echo "please login first";
-        echo "<meta http-equiv=REFRESH content='3, url=login.html'>";
+        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
     }
     else{
-        $conn=mysqli_connect("localhost","root","", "mydb");
+        $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
         $sql="insert into bulletin(title, content, type, time) 
         values('{$_POST['title']}','{$_POST['content']}', {$_POST['type']},'{$_POST['time']}')";
         if (!mysqli_query($conn, $sql)){
@@ -14,7 +14,7 @@
         }
         else{
             echo "新增佈告成功，三秒鐘後回到網頁";
-            echo "<meta http-equiv=REFRESH content='3, url=bulletin.php'>";
+            echo "<meta http-equiv=REFRESH content='3, url=11.bulletin.php'>";
         }
     }
 ?>
