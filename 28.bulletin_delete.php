73f0e8c78af0890b248b2832ba850fdd641d38a1
shhuangmust <63779526+shhuangmust@users.noreply.github.com>
2023-12-26 12:45:21 +0800

    Update 28.bulletin_delete.php

--- 28.bulletin_delete.php
+++ 28.bulletin_delete.php
@@ -3,17 +3,17 @@
     session_start();
     if (!$_SESSION["id"]) {
         echo "請登入帳號";
-        echo "<meta http-equiv=REFRESH content='3, url=login.html'>";
+        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
     }
     else{   
-        $conn=mysqli_connect("localhost","root","","mydb");
-        $sql="delete from bulletin where bid='{$_GET[bid]}'";
+        $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
+        $sql="delete from bulletin where bid='{$_GET["bid"]}'";
         #echo $sql;
         if (!mysqli_query($conn,$sql)){
             echo "佈告刪除錯誤";
         }else{
             echo "佈告刪除成功";
         }
-        echo "<meta http-equiv=REFRESH content='3, url=bulletin.php'>";
+        echo "<meta http-equiv=REFRESH content='3, url=11.bulletin.php'>";
     }
 ?>
