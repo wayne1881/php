bf0c772371c661c07fbad38128221d29d9365d4f
shhuangmust <63779526+shhuangmust@users.noreply.github.com>
2023-12-26 11:42:23 +0800

    Update 9.reset_counter.php

--- 9.reset_counter.php
+++ 9.reset_counter.php
@@ -2,6 +2,6 @@
     session_start();
     unset($_SESSION["counter"]);
     echo "counter重置成功....";
-    echo "<meta http-equiv=REFRESH content='2; url=counter.php'>";
+    echo "<meta http-equiv=REFRESH content='2; url=8.counter.php'>";
 
 ?>
