80f695f9b41c96312d037ba3d35ce779c13624c5
shhuangmust <63779526+shhuangmust@users.noreply.github.com>
2023-12-26 11:48:46 +0800

    Update 11.bulletin.php

--- 11.bulletin.php
+++ 11.bulletin.php
@@ -2,12 +2,12 @@
     error_reporting(0);
     session_start();
     if (!$_SESSION["id"]) {
-        echo "please login first";
-        echo "<meta http-equiv=REFRESH content='3, url=login.html'>";
+        echo "請先登入";
+        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
     }
     else{
-        echo "Welcome, ".$_SESSION["id"]."[<a href=logout.php>logout</a>]<br>";
-        $conn=mysqli_connect("localhost","root","", "mydb");
+        echo "歡迎登入, ".$_SESSION["id"]."[<a href=12.logout.php>logout</a>]<br>";
+        $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
         $result=mysqli_query($conn, "select * from bulletin");
         echo "<table border=2><tr><td>佈告編號</td><td>佈告類別</td><td>標題</td><td>佈告內容</td><td>發佈時間</td></tr>";
         while ($row=mysqli_fetch_array($result)){
