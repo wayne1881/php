6cd862c006451d3254a8a7ba5261ddb192dbc3fe
shhuangmust <63779526+shhuangmust@users.noreply.github.com>
2023-12-26 11:35:09 +0800

    Update 5.while.php

--- 5.while.php
+++ 5.while.php
@@ -1,6 +1,6 @@
 <?php
    #mysqli_connect() 建立資料庫連結
-   $conn=mysqli_connect("localhost","root","","mydb");
+   $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
    #mysqli_query() 從資料庫查詢資料
    $result=mysqli_query($conn, "select * from user");
    #mysqli_fetch_array() 從查詢出來的資料一筆一筆抓出來
