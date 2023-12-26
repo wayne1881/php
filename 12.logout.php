2b3488cf232659b6cd5e85ae4c249f7becebfcb7
shhuangmust <63779526+shhuangmust@users.noreply.github.com>
2023-12-26 11:49:01 +0800

    Update 12.logout.php

--- 12.logout.php
+++ 12.logout.php
@@ -2,6 +2,6 @@
     session_start();
     unset($_SESSION["id"]);
     echo "登出成功....";
-    echo "<meta http-equiv=REFRESH content='3; url=login.html'>";
+    echo "<meta http-equiv=REFRESH content='3; url=2.login.html'>";
 
 ?>
