278605abd889a5fb3ee1b3a3875b708bb5ab2363
shhuangmust <63779526+shhuangmust@users.noreply.github.com>
2023-12-26 12:10:36 +0800

    Update 19.user_edit_form.php

--- 19.user_edit_form.php
+++ 19.user_edit_form.php
@@ -6,14 +6,14 @@
     session_start();
     if (!$_SESSION["id"]) {
         echo "請登入帳號";
-        echo "<meta http-equiv=REFRESH content='3, url=login.html'>";
+        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
     }
     else{   
-        $conn=mysqli_connect("localhost", "root","","mydb");
+        $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
         $result=mysqli_query($conn, "select * from user where id='{$_GET['id']}'");
         $row=mysqli_fetch_array($result);
         echo "
-        <form method=post action=user_edit.php>
+        <form method=post action=20.user_edit.php>
             <input type=hidden name=id value={$row['id']}>
             帳號：{$row['id']}<br> 
             密碼：<input type=text name=pwd value={$row['pwd']}><p></p>
