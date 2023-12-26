de471b95807246b1b413a58163579178acab51de
shhuangmust <63779526+shhuangmust@users.noreply.github.com>
2023-12-26 12:36:46 +0800

    Update 11.bulletin.php

--- 11.bulletin.php
+++ 11.bulletin.php
@@ -6,7 +6,7 @@
         echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
     }
     else{
-        echo "歡迎, ".$_SESSION["id"]."[<a href=12.logout.php>登出</a>][<a href=14.user_add_form.php>新增使用者</a>][<a href=18.user.php>管理使用者</a>] [<a href=22.bulletin_add_form.php>新增佈告</a>]<br>";
+        echo "歡迎, ".$_SESSION["id"]."[<a href=12.logout.php>登出</a>] [<a href=18.user.php>管理使用者</a>] [<a href=22.bulletin_add_form.php>新增佈告</a>]<br>";
         $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
         $result=mysqli_query($conn, "select * from bulletin");
         echo "<table border=2><tr><td></td><td>佈告編號</td><td>佈告類別</td><td>標題</td><td>佈告內容</td><td>發佈時間</td></tr>";
