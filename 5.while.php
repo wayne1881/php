3b8239ae5366d33dd771f6affd730e54334cf3bb
wayne1802 <166781148+wayne1802@users.noreply.github.com>
2024-04-19 09:30:16 +0800

    Update 5.while.php

--- 5.while.php
+++ 5.while.php
@@ -10,3 +10,43 @@
 ?>
 login範例進階應用，判斷帳號密碼是否與資料庫資料相同
 while迴圈搭配if判斷式
+<?php
+   #mysqli_connect() 建立資料庫連結
+   $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
+   #mysqli_query() 從資料庫查詢資料
+   $result=mysqli_query($conn, "select * from user");
+   #mysqli_fetch_array() 從查詢出來的資料一筆一筆抓出來
+   $login=FALSE;
+   while ($row=mysqli_fetch_array($result)) {
+     if (($_POST["id"]==$row["id"]) && ($_POST["pwd"]==$row["pwd"])) {
+       $login=TRUE;
+     }
+   } 
+   if ($login==TRUE)
+     echo "登入成功";
+  else
+     echo "帳號/密碼 錯誤";
+?>
+佈告欄範例應用
+error_reporting(0)：取消錯誤或警告
+
+<?php
+    error_reporting(0);
+    $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
+    $result=mysqli_query($conn, "select * from bulletin");
+    echo "<table border=2><tr><td>佈告編號</td><td>佈告類別</td><td>標題</td><td>佈告內容</td><td>發佈時間</td></tr>";
+    while ($row=mysqli_fetch_array($result)){
+        echo "<tr><td>";
+        echo $row["bid"];
+        echo "</td><td>";
+        echo $row["type"];
+        echo "</td><td>"; 
+        echo $row["title"];
+        echo "</td><td>";
+        echo $row["content"]; 
+        echo "</td><td>";
+        echo $row["time"];
+        echo "</td></tr>";
+    }
+    echo "</table>"
+?>
